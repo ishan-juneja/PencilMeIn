a5618e154280a69ee5f71a4c461b988e5e5aa2ec
Melissa Shi <melissashi@ucla.edu>
2025-02-18 16:44:49 -0800

    added some styling on buttons

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -8,9 +8,9 @@ function Popup(props) {
                 <h1 className="popup-header">Pencil Me In</h1>
                 <p className="popup-text">How would you like to add your availability?</p>
                 <div className="popup-button-container">
-                    <button className="popup-button">Sign in with Google</button>
-                    <button className="popup-button">Sign in with Apple</button>
-                    <button className="popup-button" onClick = {() => props.setTrigger(false)}>Manually Input</button>
+                    <button className="popup-button" id="google">Sign in with Google</button>
+                    <button className="popup-button" id="apple">Sign in with Apple</button>
+                    <button className="popup-button" id="manual" onClick = {() => props.setTrigger(false)}>Manually Input</button>
                 </div>  
                 { props.children }
             </div>
